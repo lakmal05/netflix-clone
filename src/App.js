cbe1ef3b29e640ecb34a011e3fee433a3e8e0a37
lakmal05 <lakmaljayawardhana59@gmail.com>
2023-01-30 12:45:56 +0530

    components

---
+++ src/App.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import  Row  from './Row'
+import './App.css';
+import requests from './requests';
+import Banner from './Banner';
+import Nav from './Nav';
+
+function App() {
+    return (
+    <div className="app">
+      {/* Nav */}
+      <Nav/>
+
+      {/* Banner */}
+      <Banner />
+
+      <Row 
+        title="NETFLIX ORIGINAL" fetchUrl={requests.fetchNetflixOriginals}
+        isLargeRow
+      />
+      <Row title="Trending Now" fetchUrl={requests.fetchTrending} />
+      <Row title="Top Rated" fetchUrl={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchUrl={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchUrl={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchUrl={requests.fetchHorrorMovies} />
+      <Row title="Romance Movies" fetchUrl={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchUrl={requests.fetchDocumentaries} />
+
+    </div>
+  );
+}
+
+export default App;
+
